335dfa992f2149586f9d4ffed6a61f1b585d29c2
buphmin <buphmin@gmail.com>
2018-11-18 13:11:02 -0700

    updated endpoints

--- loadTests/getLeaguePlayer.js
+++ loadTests/getLeaguePlayer.js
@@ -2,6 +2,6 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export default function() {
-    http.get("http://127.0.0.1/index.php/league_players/1");
+    http.get("http://127.0.0.1/index.php/api/league_players/1");
     sleep(1);
 };
--- loadTests/getLeaguePlayerRaw.js
+++ loadTests/getLeaguePlayerRaw.js
@@ -3,6 +3,6 @@ import { sleep } from "k6";
 
 
 export default function() {
-    http.get("http://127.0.0.1/index.php/league_players/raw/1");
+    http.get("http://127.0.0.1/index.php/api/league_players/raw/1");
     sleep(1);
 };
